430a3cc8d6b15b102bc60d1ec73ae5d39859fcda
sugiyama1919 <nero31730503@gmail.com>
2018-01-31 16:33:46 +0900

    sava

---
+++ gamne/Assets/Script/BulletController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	public GameObject explosionPrefab; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, 0.2f, 0);
+
+		if (transform.position.y > 5) {
+			Destroy (gameObject);
+		}
+
+	}
+	void OnTriggerEnter2D(Collider2D coll) {
+		GameObject.Find ("Canvas").GetComponent<UIController> ().AddScore ();
+
+
+		GameObject effect = Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+		Destroy (effect, 1.0f);
+
+		Destroy (coll.gameObject);
+		Destroy (gameObject);
+	}
+
+}
